98322a94e9a3dcc89155cce69db2444ba7d4d661
Hydra de lerne <abdelhamedmo59@gmail.com>
2025-02-04 04:49:20 +0200

    ass

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react';
 import { Alert, SafeAreaView, StyleSheet } from 'react-native';
-import OnvoDrawingBoardView,{saveDrawing, isDrawingTooSimple} from 'onvo-drawing-board';
+import OnvoDrawingBoardView, { undoAction } from 'onvo-drawing-board';
 
 export default function App() {
   useEffect(() => {
-    const func = async () => {
-      console.log(isDrawingTooSimple())
-     const response = await saveDrawing('https://cdn.onvo.me/api/ios/','da')
-     Alert.alert('tecccst',response)
-    }
-    func()
-  },[])
+    undoAction()
+  }, [])
   return (
     <SafeAreaView style={styles.container}>
       <OnvoDrawingBoardView
